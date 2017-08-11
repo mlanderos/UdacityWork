79b14d7caa18981ce8efc1f025e833765f46bd20
Michael <michael.landeros@gmail.com>
2017-08-11 17:03:06 -0500

    Think I figured out initial solution

--- Python/MLanderos_FillintheBlanks.py
+++ Python/MLanderos_FillintheBlanks.py
@@ -1,27 +1,35 @@
 
-
-def easy_mode2():
-    print "you made it to easymode 2"
-
-def easy_mode():
-    easy_paragraph = """
-__1__ programs can be written using any text editor and should have the extention __2__.
-"""
-    max_tries = 5
-    while max_tries > 0:
-        print easy_paragraph
-        selection = raw_input('What is the answer to question 1: ').lower()
-        if selection == "python":
-            easy_paragraph = easy_paragraph.replace('__1__', selection)
-            print easy_paragraph
-            break
-        else:
-            max_tries -= 1
-            print 'wrong answer! Try again. You have ' + str(max_tries) + ' left.'
+def correct_answer(user_input, question):
+    """<>"""
+    easy_answerlist = ['python', '.py', 'yes', 'no']
+    if user_input == easy_answerlist[question - 1]:
+        return True
     else:
-        print "You failed! Max Attempt is met. Game Over!"
-        quit()
-    easy_mode2()
+        return False
 
-easy_mode()
-            
+def mode(paragraph,n):
+    """function to test each blank space from any mode"""
+    max_tries = n #expecting 5
+    question = 1
+    while question < 5: #expecting only 4 questions max
+        print paragraph
+        selection = raw_input('What is the answer to question ' + str(question) +': ').lower()
+        if max_tries > 0: #expecting 5 max tries
+            if correct_answer(selection, question):
+                paragraph = paragraph.replace('__'+ str(question) + '__', selection)
+                print "Correct!"
+                question += 1
+                max_tries = n #resetting number back for next question
+            else:
+                max_tries -= 1
+                print 'wrong answer! Try again. You have ' + str(max_tries) + ' left.'
+        else:
+            print "You Loss!! Game Over!!"
+            quit()
+    print "You WON YAY!!!"
+    quit()
+
+easy_paragraph = """
+__1__ programs can be written using any text editor and should have the extention __2__. Write Yes for __3__ and No for __4__.
+"""
+mode(easy_paragraph, 5)
