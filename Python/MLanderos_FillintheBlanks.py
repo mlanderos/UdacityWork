dd36ced91d7f8e3fa92389d9e73c69329ea0141f
Michael Landeros <michael.landeros@gmail.com>
2017-08-14 23:30:48 -0500

    removed blanks lines from project

--- Python/MLanderos_FillintheBlanks.py
+++ Python/MLanderos_FillintheBlanks.py
@@ -1,40 +1,3 @@
-
-<<<<<<< HEAD
-def correct_answer(user_input, question):
-    """<>"""
-    easy_answerlist = ['python', '.py', 'yes', 'no']
-    if user_input == easy_answerlist[question - 1]:
-        return True
-    else:
-        return False
-
-def mode(paragraph,n):
-    """function to test each blank space from any mode"""
-    max_tries = n #expecting 5
-    question = 1
-    while question < 5: #expecting only 4 questions max
-        print paragraph
-        selection = raw_input('What is the answer to question ' + str(question) +': ').lower()
-        if max_tries > 0: #expecting 5 max tries
-            if correct_answer(selection, question):
-                paragraph = paragraph.replace('__'+ str(question) + '__', selection)
-                print "Correct!"
-                question += 1
-                max_tries = n #resetting number back for next question!
-            else:
-                max_tries -= 1
-                print 'wrong answer! Try again. You have ' + str(max_tries) + ' left.'
-        else:
-            print "You Loss!! Game Over!!"
-            quit()
-    print "You WON YAY!!!"
-    quit()
-
-easy_paragraph = """
-__1__ programs can be written using any text editor and should have the extention __2__. Write Yes for __3__ and No for __4__.
-"""
-mode(easy_paragraph, 5)
-=======
 def get_answerfor(mode):
     """function to return answer key for selected mode. easy, medium, hard are the only acceptable values. Returns list"""
     if mode == 'easy':
@@ -132,4 +95,3 @@ def game_start():
         game_start()
 
 game_start()
->>>>>>> FillinBlanks
