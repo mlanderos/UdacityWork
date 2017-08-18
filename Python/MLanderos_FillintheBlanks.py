1342fda567760c6149922d1a61a8839f061b56d6
Michael <michael.landeros@gmail.com>
2017-08-18 09:00:18 -0500

    Updated file to match CodeYourOwnQuiz repository

--- Python/MLanderos_FillintheBlanks.py
+++ Python/MLanderos_FillintheBlanks.py
@@ -1,3 +1,20 @@
+def game_won(bool_result, paragraph):
+    """Input a bool value. True for when user wins the game. False if not"""
+    if bool_result:
+        print paragraph
+        print "You Won!! The force is strong with you!"
+        quit()
+    else:
+        print "You failed too many straight guesses. GAME OVER!\n"
+    quit()
+
+def islast_chance(min_c, max_c):
+    """Prints specific message dependent on how many chances remain to answer the question."""
+    if max_c > min_c:
+        print 'That isn''t the correct answer! Lets try again; You have ' + str(max_c) + ' trys left.'
+    else:
+        print 'That isn''t the correct answer! Lets try again; You have ' + str(max_c) + ' try left. Make it count!'
+
 def get_answerfor(mode):
     """function to return answer key for selected mode. easy, medium, hard are the only acceptable values. Returns list"""
     if mode == 'easy':
@@ -16,23 +33,6 @@ def verify_answer(mode, u_answer, question_num):
     else:
         return False
 
-def get_paragraphfor(mode):
-    """function to return paragraph for selected mode: easy, medium, hard are the only acceptable values. Returns string"""
-    if mode == 'easy':
-        paragraph = """
-At Udactiy we are learning how to program in __1__. The __1__ language was created by __2__ van Rossum during 1985-1990. __1__ files
- have the extention __3__. If you want to print something from Python use the __4__ command."""
-    if mode == 'medium':
-        paragraph = """
-Python __1__s do not need explicit declaration to reserve memory space. The declaration happens automatically when you __2__ 
-a value to a variable. The equal sign (=) is used to __2__ values to __1__s. The __3__ to the left of the = __4__ is the 
-name of the variable and the __3__ to the right of the = __4__ is the value stored in the variable. """
-    if mode == 'hard':
-        paragraph = """
-__1__ are the constructs which can manipulate the value of operands. Python language supports the following types of __1__:
-__2__metric __1__, Comparison __1__, __3__ment __1__, Log__4__ __1__, Bitwise __1__, Membership __1__ & Identity __1__."""
-    return paragraph
-
 def ask_question(paragraph, question):
     """Prompts user for answer to question. Returns answer in string type"""
     print "The current paragraph reads as such:"
@@ -40,37 +40,49 @@ def ask_question(paragraph, question):
     user_answer = raw_input('What is the answer to question ' + str(question) +': ').lower()
     return user_answer
 
-def islast_chance(min_c, max_c):
-    """Returns different message to user if user is on its last chance to answer question."""
-    if max_c > min_c:
-        print 'That isn''t the correct answer! Lets try again; You have ' + str(max_c) + ' trys left.'
-    else:
-        print 'That isn''t the correct answer! Lets try again; You have ' + str(max_c) + ' try left. Make it count!'
-
-def play_game(game_level, chances):
-    """logic for looping through all questions and ending game if max attempts are met. Prints string output for user"""
-    paragraph = get_paragraphfor(game_level)
-    question_overflow = 5 #expecting only 4 questions max per mode
-    question = 1
-    max_tries = chances
+def play_game(game_level, paragraph, max_chances, question_num):
+    """Runs through logic of game. Prints out whether user Won or Lost."""
+    question_overflow = 5
     min_tries = 1
-    while question < question_overflow:
-        user_answer = ask_question(paragraph, question)
+    max_tries = max_chances
+    while question_num < question_overflow:
+        user_answer = ask_question(paragraph, question_num)
         if max_tries > min_tries:
-            if verify_answer(game_level, user_answer, question):
-                paragraph = paragraph.replace('__'+ str(question) + '__', user_answer)
+            if verify_answer(game_level, user_answer, question_num):
+                paragraph = paragraph.replace('__'+ str(question_num) + '__', user_answer)
                 print "Correct!\n"
-                question += 1
-                max_tries = chances #resetting retries back to chances variable for next question!
+                question_num += 1
+                max_tries = max_chances #resetting quesses for next question!
             else:
                 max_tries -= 1
                 islast_chance(min_tries, max_tries)
         else:
-            print "You failed too many straight guesses. GAME OVER!\n"
-            quit()
-    print paragraph
-    print "You WON YAY!!!"
-    quit()
+            game_won(False, paragraph)
+    game_won(True, paragraph)
+
+def get_paragraphfor(mode):
+    """function to return paragraph for selected mode: easy, medium, hard are the only acceptable values. Returns string"""
+    if mode == 'easy':
+        paragraph = """
+At Udactiy we are learning how to program in __1__. The __1__ language was created by __2__ van Rossum during 1985-1990. __1__ files
+ have the extention __3__. If you want to print something from Python use the __4__ command.\n"""
+    if mode == 'medium':
+        paragraph = """
+Python __1__s do not need explicit declaration to reserve memory space. The declaration happens automatically when you __2__ 
+a value to a variable. The equal sign (=) is used to __2__ values to __1__s. The __3__ to the left of the = __4__ is the 
+name of the variable and the __3__ to the right of the = __4__ is the value stored in the variable.\n"""
+    if mode == 'hard':
+        paragraph = """
+__1__ are the constructs which can manipulate the value of operands. Python language supports the following types of __1__:
+__2__metric __1__, Comparison __1__, __3__ment __1__, Log__4__ __1__, Bitwise __1__, Membership __1__ & Identity __1__.\n"""
+    return paragraph
+
+def get_gamedetails(game_level, chances):
+    """logic for looping through all questions and ending game if max attempts are met. Prints string output for user\n"""
+    paragraph = get_paragraphfor(game_level)
+    max_chances = chances
+    question_num = 1
+    play_game(game_level, paragraph, max_chances, question_num)
 
 def guesses():
     """function for user to input a integer for how mnay guesses he/she has to play each question. Returns int"""
@@ -82,8 +94,8 @@ def guesses():
     else:
         max_limit = 100
         if isinstance(tries, int):
-            if tries > max_limit:
-                print "Sorry number is not lower than 100. Pick new number"
+            if tries > max_limit or tries == 0:
+                print "Sorry number is not in the range of 1 to 99. Pick new number"
                 guesses()
             else:
                 print "You will get " + str(tries) + " guesses per problem.\n"
@@ -97,7 +109,7 @@ def game_start():
     if user_mode in modes:
         print "You chose game mode: " + user_mode
         user_guess = guesses()
-        play_game(user_mode, user_guess)
+        get_gamedetails(user_mode, user_guess)
     else:
         print "Incorrect. Please check your spelling and try again."
         game_start()
